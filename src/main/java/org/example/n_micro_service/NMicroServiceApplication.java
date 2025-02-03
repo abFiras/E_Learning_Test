24a46926b956d1c23579c6cbe54c0b0ae91dc302
abFiras <firas.abdallah@esprit.tn>
2025-02-03 11:50:56 +0100

    first commit

---
+++ src/main/java/org/example/n_micro_service/NMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.n_micro_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/n_micro_service/NMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.n_micro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
